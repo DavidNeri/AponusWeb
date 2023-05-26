4ce60045a9c475f31ff67832802524de54869a11
David Christian Neri <50834481+DavidNeri@users.noreply.github.com>
2023-05-26 18:52:32 -0300

    NuevoMetodos Metodos_TablasNuevas

--- Aponus Web API/Data Transfer Objects/GuardarProducto.cs
+++ Aponus Web API/Data Transfer Objects/GuardarProducto.cs
@@ -1,11 +1,14 @@
 ﻿using Aponus_Web_API.Data_Transfer_objects;
+using Newtonsoft.Json;
 
 namespace Aponus_Web_API.Data_Transfer_Objects
 {
     public class GuardarProducto
     {
+        [JsonProperty(PropertyName = "producto", NullValueHandling = NullValueHandling.Ignore)]
         public DatosProducto Producto{ get; set; }
 
+        [JsonProperty(PropertyName = "componentesProducto", NullValueHandling = NullValueHandling.Ignore)]
         public List<DTOComponentesProducto> Componentes { get; set; }
 
     }
