96daf1e7f5394df64ff929b3585d319790e98069
David Christian Neri <50834481+DavidNeri@users.noreply.github.com>
2024-09-09 09:04:38 -0300

    Modificacion DockerFile

--- Aponus Web API/obj/Debug/net7.0/Aponus Web API.AssemblyInfo.cs
+++ Aponus Web API/obj/Debug/net7.0/Aponus Web API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aponus Web API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e50addf2f29e332b1565aa044c3b34c728fb6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a762d29ec881f63811d500f1a9bdfbb085043ef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aponus Web API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aponus Web API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
