c1814db9d2962944598286497d8172df7988d021
David Christian Neri <50834481+DavidNeri@users.noreply.github.com>
2025-02-22 20:10:50 -0300

    Correciones2

--- Aponus Web API/obj/Debug/net7.0/Aponus Web API.AssemblyInfo.cs
+++ Aponus Web API/obj/Debug/net7.0/Aponus Web API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aponus Web API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c58b485c6181916cec1b45b23e87824b7479bdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8a5ebf334e5aac2ed246825ec7bcf754bd2a7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aponus Web API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aponus Web API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
